d31d7e0aee45c553916fe65fa55b74e143bd0d01
Astra Bertelli <133636879+AstraBert@users.noreply.github.com>
2024-01-23 21:10:54 +0100

    Update user_case.py

--- user_case.py
+++ user_case.py
@@ -5,8 +5,18 @@ This project walks you through building a simple, user-customizable, Telegram bo
 from telegram.ext import *
 import emoji
 from jan_scraper.scraper import scrape_jan
+import sys
 
 TOKEN = ""
+
+jan_app = ""
+jan_threads = ""
+instructions = ""
+assistant_name = ""
+llm_model = ""
+new_thread = True
+
+
 
 async def start_commmand(update, context):
     await update.message.reply_text(emoji.emojize(f""))
@@ -14,9 +24,38 @@ async def start_commmand(update, context):
 async def help_command(update, context):
     await update.message.reply_text(emoji.emojize(f""))
 
+async def display_available_models_command(update, context):
+    await update.message.reply_text(emoji.emojize(f"Here are all the models you can use:\n\n-tinyllama-1.1b: For starters, the tiniest llama on Earth will be helping you for basic requests...:llama:"))
+
+async def choose_model_command(update, context):
+    global llm_model
+    text =  update.message.text
+    t = text.replace("/model","")
+    llm_model = t
+    await update.message.reply_text(emoji.emojize(f"All right, you'll be using {llm_model} from now on!:robot:"))
+
+async def conversation_mode_command(update, context):
+    global new_thread
+    new_thread = False
+    await update.message.reply_text(emoji.emojize(f"Coversation mode activated!:rocket:"))
+
+async def instructions_command(update, context):
+    global instructions
+    text =  update.message.text
+    t = text.replace("/model","")
+    instructions = t
+    await update.message.reply_text(emoji.emojize(f"Perfect, assistant is now set as: {instructions}"))
+
+async def name_command(update, context):
+    global assistant_name
+    text =  update.message.text
+    t = text.replace("/model","")
+    assistant_name = t
+    await update.message.reply_text(emoji.emojize(f"Cool! Your assistant will be named {assistant_name}:partying_face:"))
+
 async def unrecognized_command(update,context):
     text =  update.message.text
-    if text.startswith("/start")==False or text.startswith("/help")==False:
+    if text.startswith("/start")==False or text.startswith("/help")==False or text.startswith("/model")==False or text.startswith("/conversation")==False or text.startswith("/instructions")==False or text.startswith("/name") or text.startswith("models_list"):
         await update.message.reply_text(f"I cannot understand the message:\n\"{text}\"\nAs my programmer did not insert it among the command I am set to respond: please check for misspelling/errors or contact the programmer if you feel anything is wrong/missing")
     else:
         pass
@@ -34,6 +73,11 @@ if __name__ == '__main__':
         # Commands
         application.add_handler(CommandHandler('start', start_commmand))
         application.add_handler(CommandHandler('help', help_command))
+        application.add_handler(CommandHandler('name', name_command))
+        application.add_handler(CommandHandler('conversation', conversation_mode_command))
+        application.add_handler(CommandHandler('instructions', instructions_command))
+        application.add_handler(CommandHandler('model', choose_model_command))
+        application.add_handler(CommandHandler('models_list', display_available_models_command))
         application.add_handler(MessageHandler(filters.COMMAND, unrecognized_command))
         application.add_error_handler(error_handler)
         # Run bot
