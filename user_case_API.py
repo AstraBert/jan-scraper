edcf977e4c18f1b9db60d0d8abbb970cc6259e8b
Astra Bertelli <133636879+AstraBert@users.noreply.github.com>
2024-01-28 20:59:19 +0100

    Update user_case_API.py

--- user_case_API.py
+++ user_case_API.py
@@ -4,6 +4,7 @@ The result will be a Telegram bot that will respond to your code inquiries and w
 
 
 from jan_scraper.scraper import scrape_jan_through_api
+from jan_scraper.formatter import convert_code_to_curl_json
 from telegram.ext import *
 import os
 import emoji
@@ -40,22 +41,6 @@ async def help_command(update, context):
     """Handle the /help command."""
     await update.message.reply_text(emoji.emojize(f"Examples of messages you can send:\n\n:linked_paperclips::page_facing_up:bot.py\n(this is to refactor the code in bot.py)\n\nEhi, can you tell me how to print a string in python?"))
 
-def convert_code_to_curl_json(code):
-    """
-    Convert a Python code string to a format suitable for inclusion in a JSON string within a curl command.
-
-    Parameters:
-    - code (str): Python code string.
-
-    Returns:
-    - str: JSON-formatted string suitable for inclusion in a curl command.
-    """
-    # Escape backslashes and double quotes in the code
-    escaped_code = code.replace('"', '\\\\\\\"')
-
-    # Replace newline characters with '\\n'
-    formatted_code = escaped_code.replace('\n', '\\\\n')
-    return formatted_code
 
 def get_python_code(lines):
     """Extract Python code blocks from a list of lines."""
