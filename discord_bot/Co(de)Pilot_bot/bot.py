b65b32461ea88fff6a16d7c3d86a9320d73a5c6f
Astra Bertelli <133636879+AstraBert@users.noreply.github.com>
2024-01-28 21:00:21 +0100

    Update bot.py

--- discord_bot/Co(de)Pilot_bot/bot.py
+++ discord_bot/Co(de)Pilot_bot/bot.py
@@ -1,29 +1,12 @@
 from discord import Client, File, Intents
 from jan_scraper.scraper import scrape_jan_through_api
+from jan_scraper.formatter import convert_code_to_curl_json
 import os
 import sqlite3
 import time
 
 CHANNEL_ID = 0
 TOKEN = ""
-
-
-def convert_code_to_curl_json(code):
-    """
-    Convert a Python code string to a format suitable for inclusion in a JSON string within a curl command.
-
-    Parameters:
-    - code (str): Python code string.
-
-    Returns:
-    - str: JSON-formatted string suitable for inclusion in a curl command.
-    """
-    # Escape backslashes and double quotes in the code
-    escaped_code = code.replace('"', '\\\\\\"')
-
-    # Replace newline characters with '\\n'
-    formatted_code = escaped_code.replace("\n", "\\\\n")
-    return formatted_code
 
 
 def get_python_code(lines):
